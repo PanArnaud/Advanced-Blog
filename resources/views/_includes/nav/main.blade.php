1853cce33f77b7101511640bae52dbe151263560
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2017-07-26 23:58:52 +0400

    delete comment

--- resources/views/_includes/nav/main.blade.php
+++ resources/views/_includes/nav/main.blade.php
@@ -36,11 +36,6 @@
                   Logout
                 </a>
                 @include('_includes.forms.logout')
-                {{-- <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form> --}}
-
              </li>
           </ul>
         </button>
